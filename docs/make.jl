7335046dcf693b74950888640ac75e221dbbe375
Youssef Miftah <miftahyo@outlook.fr>
2025-11-25 20:35:33 +1100

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,27 @@
 using AustralianElectricityMarkets
 using Documenter
+using Literate
+
+# This code performs the automated addition of Literate - Generated Markdowns.
+julia_file_filter = x -> occursin(".jl", x)
+outputdir = joinpath(pwd(), "docs", "src", "examples")
+files = filter(julia_file_filter, readdir(outputdir))
+
+for file in files
+    @show file
+    inputfile = joinpath(outputdir, "$file")
+    outputfile = replace("$file", ".jl" => "")
+
+    Literate.markdown(
+        inputfile,
+        outputdir;
+        name = outputfile,
+        # credit = false,
+        flavor = Literate.DocumenterFlavor(),
+        # documenter = true,
+        execute = true,
+    )
+end
 
 DocMeta.setdocmeta!(
     AustralianElectricityMarkets,
@@ -17,7 +39,12 @@ makedocs(;
         edit_link = "main",
         assets = String[],
     ),
-    pages = ["Home" => "index.md"],
+    pages = [
+        "Home" => "index.md",
+        "Getting started" => [
+            "Explore the NEM" => "examples/build_system.jl",
+        ],
+    ],
 )
 
 deploydocs(; repo = "github.com/ymiftah/AustralianElectricityMarkets.jl", devbranch = "main")
