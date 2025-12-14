82922261c7e65cc303d019cf07245d02a698a952
Youssef Miftah <miftahyo@outlook.fr>
2025-12-14 11:37:33 +1100

    reorder imports

--- src/AustralianElectricityMarkets.jl
+++ src/AustralianElectricityMarkets.jl
@@ -17,25 +17,27 @@ export read_interconnectors, read_units, read_demand, read_affine_heatrates,
     read_coal_prices, read_gas_prices, read_biomass_prices, read_isp_thermal_costs_parameters
 export set_demand!, set_renewable_pv!, set_renewable_wind!
 
-include("AustralianElectricityMarketsData.jl")
 
 # Write your package code here.
 include("constants.jl")
 include("configurations.jl")
 include("geoutils.jl")
+include("network_models/interface.jl")
+
+
+include("AustralianElectricityMarketsData.jl")
+
+# Export data module
+using .AustralianElectricityMarketsData: PyHiveConfiguration, fetch_table_data, list_available_tables, read_affine_heatrates,
+    read_coal_prices, read_gas_prices, read_biomass_prices, read_isp_thermal_costs_parameters
 
 # Modules
 include("parser.jl")
 
 # Parsing data into models
-include("network_models/interface.jl")
 include("network_models/region_model.jl")
 
 # Exports the network models implemented
 using .RegionModel: RegionalNetworkConfiguration
-
-# Export data module
-using .AustralianElectricityMarketsData: PyHiveConfiguration, fetch_table_data, list_available_tables, read_affine_heatrates,
-    read_coal_prices, read_gas_prices, read_biomass_prices, read_isp_thermal_costs_parameters
 
 end
