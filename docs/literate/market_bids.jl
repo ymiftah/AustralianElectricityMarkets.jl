717b456b8feef6a44658a1f2cb6b56921ac20bc6
Youssef Miftah <miftahyo@gmail.com>
2026-03-08 22:51:02 +1100

    docs: add Literate source for market_bids example

---
+++ docs/literate/market_bids.jl
@@ -0,0 +1,177 @@
+using AustralianElectricityMarkets
+using PowerSystems
+using PowerSimulations
+using HydroPowerSimulations
+using Chain
+using DataFrames
+using AlgebraOfGraphics, CairoMakie
+using TidierDB
+using Dates
+using HiGHS
+
+# # Market Clearing
+#
+# In the NEM, generators submit bids consisting of up to **10 price-quantity bands** for
+# each 5-minute dispatch interval. Prices can range from the market floor (currently -$1,000/MWh)
+# to the market price cap (currently $17,500/MWh).
+#
+# The following section demonstrates the definition of a market clearing problem, where
+# all units in the NEM need to be dispatched according to their energy bids to meet the aggregate
+# demand at each region. In this example, we still ignore network constraints to simplify the problem,
+# and adopt a "Copper Plate" formulation.
+#
+# ## Setup the system
+#
+# Initialise a connection to manage the market data via duckdb.
+#
+# !!! note "Get the data first!"
+#     You will first need to download the data from the monthly archive, saving them locally
+#     in parquet files.
+#
+# ```julia
+# tables = table_requirements(RegionalNetworkConfiguration())
+# map(tables) do table
+#     fetch_table_data(table, date_range)
+# end;
+# ```
+#
+# Only the data requirements for a `RegionalNetworkConfiguration` are downloaded.
+
+db = aem_connect(duckdb());
+nothing #hide
+
+# Instantiate the system.
+
+sys = nem_system(db, RegionalNetworkConfiguration())
+
+# Set the horizon to consider for the simulation.
+
+interval = Minute(5)
+horizon = Hour(24)
+start_date = DateTime(2025, 1, 2, 4, 0)
+date_range = start_date:interval:(start_date + horizon)
+@show date_range
+
+# Set deterministic time series.
+
+set_demand!(sys, db, date_range; resolution = interval)
+set_renewable_pv!(sys, db, date_range; resolution = interval)
+set_renewable_wind!(sys, db, date_range; resolution = interval)
+set_hydro_limits!(sys, db, date_range; resolution = interval)
+set_market_bids!(sys, db, date_range)
+
+# Derive forecasts from the deterministic time series.
+
+transform_single_time_series!(sys, horizon, interval);
+@show sys
+
+# ## Define the problem
+
+begin
+    template = ProblemTemplate()
+    set_device_model!(template, Line, StaticBranch)
+    set_device_model!(template, PowerLoad, StaticPowerLoad)
+    set_device_model!(template, RenewableDispatch, RenewableFullDispatch)
+    set_device_model!(template, ThermalStandard, ThermalBasicUnitCommitment)
+    set_device_model!(template, HydroDispatch, HydroDispatchRunOfRiver)
+    set_network_model!(template, NetworkModel(CopperPlatePowerModel))
+    template
+end
+
+# The dispatch problem will be solved with open source solver HiGHS.
+
+solver = optimizer_with_attributes(HiGHS.Optimizer, "mip_rel_gap" => 0.05)
+problem = DecisionModel(template, sys; optimizer = solver, horizon = horizon)
+build!(problem; output_dir = joinpath(tempdir(), "out"))
+
+# Solve the problem.
+
+solve!(problem)
+
+# Observe the results.
+
+res = OptimizationProblemResults(problem)
+
+# Let's observe how the units are dispatched.
+
+function filter_non_all_zero(df, group_by)
+    gdf = groupby(df, group_by)
+    is_all_zero = combine(gdf, :value => (x -> all(x == 0)) => :all_zero)
+    subset!(is_all_zero, :all_zero => x -> .!x)
+    return innerjoin(df, is_all_zero, on = group_by)
+end
+
+begin
+    renewables = read_variable(res, "ActivePowerVariable__RenewableDispatch")
+    thermal = read_variable(res, "ActivePowerVariable__ThermalStandard")
+    hydro = read_variable(res, "ActivePowerVariable__HydroDispatch")
+    gens_long = vcat(renewables, thermal, hydro)
+    select!(gens_long, :DateTime, :name => :DUID, :value)
+
+    by_fuel = @chain select(
+        read_units(db),
+        [:DUID, :STATIONID, :CO2E_ENERGY_SOURCE, :REGIONID]
+    ) begin
+        rightjoin(gens_long, on = :DUID)
+        groupby([:CO2E_ENERGY_SOURCE, :REGIONID, :DateTime])
+        combine(:value => sum => :value)
+        dropmissing!
+        select!(:DateTime, :REGIONID, :CO2E_ENERGY_SOURCE => :Source, :value)
+        filter_non_all_zero([:REGIONID, :Source])
+    end
+
+    loads = @chain res begin
+        read_parameter("ActivePowerTimeSeriesParameter__PowerLoad")
+        transform!(:name => ByRow(x -> split(x, " ")[1]) => :REGIONID)
+        subset!(:REGIONID => ByRow(!=("SNOWY1")))
+        insertcols!(:Source => "Region demand")
+        select!(:DateTime, :REGIONID, :Source, :value => ByRow(-) => :value)
+    end
+
+    demand = data(loads) * mapping(:DateTime, :value, color = :Source, layout = :REGIONID) * visual(Lines, linestyle = (:dash, :dense))
+    generation = data(by_fuel) * mapping(:DateTime, :value, color = :Source, layout = :REGIONID) * visual(Lines)
+    draw(demand + generation; figure = (; size = (1000, 800)), legend = (; position = :bottom))
+end
+
+# Let's observe the dispatch of a few thermal generators.
+
+begin
+    thermals_non_zero = filter_non_all_zero(thermal, :name)
+    sample = first(unique(thermals_non_zero.name), 5)
+    sample = subset!(thermals_non_zero, :name => ByRow(in(sample)))
+    spec = data(sample) * mapping(:DateTime, :value, color = :name) * visual(Lines)
+    draw(spec; figure = (; size = (500, 500)))
+end
+
+# Let's observe the dispatch of a few renewable generators.
+
+begin
+    renewables_non_zero = filter_non_all_zero(renewables, :name)
+    sample = first(unique(renewables_non_zero.name), 5)
+    sample = subset!(renewables_non_zero, :name => ByRow(in(sample)))
+    data(sample) * mapping(:DateTime, :value, color = :name) * visual(Lines) |> draw
+end
+
+# Notice that most solar generators are actually **not** dispatched during the day here even
+# though the solar output is definitely non-zero.
+
+begin
+    ren = get_component(RenewableDispatch, sys, "COLEASF1")
+    ts = get_time_series_array(Deterministic, ren, "max_active_power") |> DataFrame
+    index, values = ts.timestamp, ts.A * 100
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel = "timestamp", title = "Max active power [MW]", ylabel = "MW")
+    lines!(index, values; label = "COLEASF1")
+    axislegend(ax)
+    fig
+end
+
+# This was not observed in the Economic dispatch example, and many factors can explain this behaviour:
+#
+# - **Negative Bidding**: In the NEM, many generators (especially coal) have high "cycling costs":
+#   it is expensive for them to turn off and restart. To avoid this, they often bid at very low or
+#   even negative prices, ensuring they are dispatched even when demand is low or renewables are abundant.
+# - **Contractual Obligations**: Some generators may have hedged their risk with future contracts,
+#   requiring them to generate regardless of the spot price.
+# - **Strategic Behavior**: The 10-band bidding system allows for complex strategies that simple
+#   cost-minimization models do not capture.
